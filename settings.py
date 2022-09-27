6b7ef32c42a7ac7919205b6943113ea270166c2c
Pia Probst <pia.probst@online.de>
2022-09-27 17:17:20 +0200

    next attempt3

--- settings.py
+++ settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'bedrohteTiere.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
     {
@@ -77,7 +77,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'bedrohteTiere.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database
