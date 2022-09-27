150af62a7a37631e2945672c9290cd3b1f39941e
Pia Probst <pia.probst@online.de>
2022-09-27 16:47:59 +0200

    new attempt remove folder

--- bedrohteTiere/wsgi.py
+++ bedrohteTiere/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bedrohteTiere.bedrohteTiere.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bedrohteTiere.settings')
 
 application = get_wsgi_application()
